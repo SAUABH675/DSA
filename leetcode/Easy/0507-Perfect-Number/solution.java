4ab2a818b1981c708c0fbc6849c442533642abc0
Saurabh Tiwari <145242079+SAUABH675@users.noreply.github.com>
2026-09-24 22:27:45 +0530

    Add LeetCode Easy - Perfect Number

---
+++ leetcode/Easy/0507-Perfect-Number/solution.java
@@ -0,0 +1,20 @@
+// LeetCode Problem: Perfect Number
+// Link: https://leetcode.com/problems/perfect-number/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1; i<=num/2; i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(num==sum){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
