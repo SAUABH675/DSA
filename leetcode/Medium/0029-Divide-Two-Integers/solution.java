8f38e19581ca7504ec55095dc68559143d42dc4c
Saurabh Tiwari <145242079+SAUABH675@users.noreply.github.com>
2026-09-24 22:27:39 +0530

    Add LeetCode Medium - Divide Two Integers

---
+++ leetcode/Medium/0029-Divide-Two-Integers/solution.java
@@ -0,0 +1,37 @@
+// LeetCode Problem: Divide Two Integers
+// Link: https://leetcode.com/problems/divide-two-integers/
+// Difficulty: Medium
+// Language: java
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+
+        long quotient = 0;
+
+        while (a >= b) {
+            long temp = b;
+            long multiple = 1;
+
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            a -= temp;
+            quotient += multiple;
+        }
+
+        if (negative) quotient = -quotient;
+
+        return (int) quotient;
+    }
+}
