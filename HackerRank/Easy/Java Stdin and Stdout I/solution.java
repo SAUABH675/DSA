4cea4dbc1b09999175634d0f9c519e8d6cd92ecd
Saurabh Tiwari <145242079+SAUABH675@users.noreply.github.com>
2026-09-24 22:26:29 +0530

    Add HackerRank Easy -  Java Stdin and Stdout I

---
+++ HackerRank/Easy/Java Stdin and Stdout I/solution.java
@@ -0,0 +1,24 @@
+// HackerRank Problem:  Java Stdin and Stdout I
+// Link: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+// Difficulty: Easy
+// Language: java
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b=scan.nextInt();
+        // Complete this line
+        int c=scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+}
