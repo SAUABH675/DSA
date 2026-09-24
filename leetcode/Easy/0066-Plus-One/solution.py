a5cf11f4200fc48717221382904d8efca2c6677f
Saurabh Tiwari <145242079+SAUABH675@users.noreply.github.com>
2026-09-24 22:28:26 +0530

    Add LeetCode Easy - Plus One

---
+++ leetcode/Easy/0066-Plus-One/solution.py
@@ -0,0 +1,20 @@
+# LeetCode Problem: Plus One
+# Link: https://leetcode.com/problems/plus-one/
+# Difficulty: Easy
+# Language: python
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
+        
+        
