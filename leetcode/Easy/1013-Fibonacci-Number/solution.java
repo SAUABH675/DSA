66a1199620f6e2773876f129313917e23d5ec6b1
Saurabh Tiwari <145242079+SAUABH675@users.noreply.github.com>
2026-09-25 22:54:52 +0530

    Add LeetCode Easy - Fibonacci Number

---
+++ leetcode/Easy/1013-Fibonacci-Number/solution.java
@@ -0,0 +1,23 @@
+// LeetCode Problem: Fibonacci Number
+// Link: https://leetcode.com/problems/fibonacci-number/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int fib(int n) {
+        int num1=0;
+        int num2=1;
+        int sum=0;
+        if(n==0){
+            return 0;
+        }if(n==1){
+            return 1;
+        }
+        for(int i=1; i<n; i++){
+            sum=num1+num2;
+            num1=num2;
+            num2=sum;
+        }
+        return sum;
+    }
+}
