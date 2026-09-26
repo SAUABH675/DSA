87a0d528b174322887673713c0cba50d0752bd51
Saurabh Tiwari <145242079+SAUABH675@users.noreply.github.com>
2026-09-26 15:44:00 +0530

    Add LeetCode Easy - N-th Tribonacci Number

---
+++ leetcode/Easy/1236-N-Th-Tribonacci-Number/solution.java
@@ -0,0 +1,29 @@
+// LeetCode Problem: N-th Tribonacci Number
+// Link: https://leetcode.com/problems/n-th-tribonacci-number/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int tribonacci(int n) {
+        int num1=0;
+        int num2=1;
+        int num3=1;
+        int sum=0;
+        if(n==0){
+            return 0;
+        }if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 1;
+        }
+        for(int i=2; i<n; i++){
+            sum=num1+num2+num3;
+            num1=num2; 
+            num2=num3;
+            num3=sum;
+        }
+        return sum;
+        
+    }
+}
